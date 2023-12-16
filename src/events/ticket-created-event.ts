39167cba59f0274772a42bf239f43142046bcc25
Aditya Bhattad <adityabhattad18@gmail.com>
2023-12-16 22:38:14 +0530

    add ticket created event interface

---
+++ src/events/ticket-created-event.ts
@@ -0,0 +1,11 @@
+import { Subjects } from "./subjects";
+
+export interface TicketCreatedEvent {
+    subject:Subjects.TicketCreated;
+    data:{
+        id:string;
+        title:string;
+        userId:string;
+        price:number;
+    }
+}
