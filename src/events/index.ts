ff3e5deb32f47b5e83278e6131db8ecb85bfc85c
Aditya Bhattad <adityabhattad18@gmail.com>
2023-12-16 22:39:46 +0530

    add path to the index

---
+++ src/events/index.ts
@@ -0,0 +1,6 @@
+import { Listner } from "./nats-listner";
+import { Publisher } from "./nats-publisher";
+import { Subjects } from "./subjects";
+import { TicketCreatedEvent } from "./ticket-created-event";
+
+export { TicketCreatedEvent, Subjects, Publisher, Listner };
